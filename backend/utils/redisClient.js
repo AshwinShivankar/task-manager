90267eab7cdd8e6475895929f9bd3c65eabc231c
ashwin <ashwin.msh21@gmail.com>
2025-05-17 20:51:53 +0530

    updated

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -1,10 +1,12 @@
 const redis = require("redis");
 
 // Create Redis client
-const redisClient = redis.createClient({
-  url: "redis://localhost:6379", // Default Redis port
+const redisClient = createClient({
+  socket: {
+    host: "127.0.0.1", // or 'localhost'
+    port: 6379,
+  },
 });
-
 redisClient.on("error", (err) => {
   console.error("Redis connection error:", err);
 });
